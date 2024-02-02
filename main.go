11963ce788a95258823c1ba7cb0faa7d78fc28d3
Jimmie <jimmie@MacBook-Pro-56.local>
2024-02-02 13:47:54 -0500

    initial commit

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"io"
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+
+	router.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK, "Welcome to our video streaming platform!")
+	})
+
+	router.GET("/stream/:filename", func(c *gin.Context) {
+		filename := c.Param("filename")
+		file, err := os.Open("videos/" + filename)
+		if err != nil {
+			c.String(http.StatusNotFound, "Video not found.")
+			return
+		}
+		defer file.Close()
+
+		c.Header("Content-Type", "video/mp4")
+		buffer := make([]byte, 64*1024) // 64KB buffer size
+		io.CopyBuffer(c.Writer, file, buffer)
+	})
+
+	router.Run(":8080")
+}
